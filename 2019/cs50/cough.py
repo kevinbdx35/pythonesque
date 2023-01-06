2956060634b391d432c961cf99182e29e24140d1
Kévin <56440487+kevinbdx35@users.noreply.github.com>
2023-01-06 17:46:45 +0100

    Create cough.py

---
+++ 2019/cs50/cough.py
@@ -0,0 +1,36 @@
+print("cough")
+print("cough")
+print("cough")
+
+####
+
+for i in [0, 1, 2]:
+		print("cough")
+    
+###
+
+for i in range(3):
+		print("cough")
+    
+###
+
+for i in range(3):
+		cough()
+
+def cough():
+		print("cough")
+
+###
+
+def main():
+				cough(3)
+
+def cough(n):
+		for i in range(n):
+				print("cough")
+
+main()
+
+###
+
+print("cough\n" * 3)
