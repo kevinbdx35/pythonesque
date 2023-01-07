4d2fdfa02b9d5c8f14e8dbaca618a18d6c1753b1
Kévin <56440487+kevinbdx35@users.noreply.github.com>
2023-01-07 18:22:01 +0100

    Create hello_world.py

---
+++ 2023/100days/day1/hello_world.py
@@ -0,0 +1,14 @@
+#Ici nous allons juste imprimer un texte qui dit Hello World!
+#Les doubles quotes/guillements qui encadrent le texte Hello World sont ici 
+#justement pour preciser que c est juste du texte que nous imprimons a l ecran.
+#Ils montrent essentiellement le debut et la fin de cette chaine de caracteres.
+#Les morceaux de texte dans le jargon de la programmation sont connus
+#sous le nom de Strings, c est une suite de caracteres plus courament appelee
+#chaine de caracteres.
+print("Hello World!")
+#Les doubles guillements peuvent etre remplacer par des simples, il faut juste rester 
+#coherent. Neanmoins les doubles guillements sont a previligier 
+print('Hello World!')
+
+#nouvelle exemple
+print("print('what to print')")
