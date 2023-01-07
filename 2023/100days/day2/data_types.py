a1971e6387d63b381ef94c9493843bef80870150
Kévin <56440487+kevinbdx35@users.noreply.github.com>
2023-01-07 18:25:44 +0100

    Create data_types.py

---
+++ 2023/100days/day2/data_types.py
@@ -0,0 +1,23 @@
+#Data types
+
+#string
+#Methode pour extraire juste une lettre d un string
+#ici on extrait la lettre H
+print("Hello"[0])
+
+print(len("toto"))
+
+#integer
+print(123 + 345)
+
+#Dans Python pour mieux comprendre et lire les grands nombres,
+#nous pouvons les écrires en mettant des underscores
+#exemple avec le nombre de 342 654 896,
+#nous pouvons l'écire comme ci-dessous
+print(342_654_896)
+
+#float
+print(3.14159)
+
+#boolean
+#True or False
