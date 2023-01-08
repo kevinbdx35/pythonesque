bd53fddc080408040f8c7032bc439a614d16007e
Kévin <56440487+kevinbdx35@users.noreply.github.com>
2023-01-08 15:44:43 +0100

    Update range.py

--- 2023/100days/day5/range.py
+++ 2023/100days/day5/range.py
@@ -5,3 +5,17 @@ total = 0
 for number in range(1, 101):
   total += number
 print(total)
+
+
+total = 0
+for number in range(2, 101, 2):
+    print(number)
+    total += number
+print(total)
+
+total2 = 0
+for number in range(1, 101):
+    if number % 2 == 0:
+        total2 += number
+
+print(total2)       
