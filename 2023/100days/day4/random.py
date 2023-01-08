97a5aa3ca67a048883798ce743a5dcf53c2c72ce
Kévin <56440487+kevinbdx35@users.noreply.github.com>
2023-01-08 14:54:34 +0100

    Update random.py

--- 2023/100days/day4/random.py
+++ 2023/100days/day4/random.py
@@ -3,5 +3,12 @@ import random
 random_integer = random.randint(1, 10)
 print(random_integer)
 
+#0.00000000 - 0.99999...
 random_float = random.random()
 print(random_float)
+
+randomfloat = random.random() * 5
+print(randomfloat)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
